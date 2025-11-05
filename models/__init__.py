b1b3a2ca15f74a291ef7e505e290caf74a9fbe1e
Daniele Senko <142120811+daniele-senko@users.noreply.github.com>
2025-11-05 01:12:37 +0000

    build: Adicionar __init__.py ao pacote models
    Cria o arquivo models/__init__.py.
    
    Este arquivo é necessário para que o Python trate o diretório 'models'
    como um pacote. Isso permite que usemos importações relativas
    (ex: 'from .pessoa import Pessoa') entre os arquivos de modelo.

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+# Este arquivo __init__.py é necessário para que o Python
+# trate o diretório 'models' como um "pacote".
+#
+# Isso nos permite usar importações relativas, como:
+# from .pessoa import Pessoa
+#
+# Pode ficar vazio.
