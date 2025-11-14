964b563b3beb924c794a6d89bb0e94a4a6053af9
Daniele Senko <daniele.senko@unemat.br>
2025-11-14 18:29:39 -0400

    chore: limpa os arquivos desnecessários e do merge conflituoso anterior

--- clinica_app/__init__.py
+++
@@ -1,7 +0,0 @@
-# Este arquivo __init__.py é necessário para que o Python
-# trate o diretório 'models' como um "pacote".
-#
-# Isso nos permite usar importações relativas, como:
-# from .pessoa import Pessoa
-#
-# Pode ficar vazio.
